9558af70a2ee09b66f15115d324e538b91dce813
My Kim Bui <mykimbuibui@gmail.com>
2017-03-22 11:29:27 +0100

    add cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+   config.cloud_name = 'alissadw'
+   config.api_key = '431757488493335'
+   config.api_secret = 'qxu6ELSWXsOSRl165BpMnG60qd8'
+   config.cdn_subdomain = true
+ end
